7858afe8515e247f05645022a25745c61e4674d9
Chris <christopher.sarm15@gmail.com>
2022-06-08 14:19:47 -0400

    home page styling

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import profilePicture from '../../assets/images/chris_pic.jpg';
-
+import React from "react";
+import profilePicture from "../../assets/images/chris_pic.jpg";
 
 function Home() {
-    return (
-      <section className='flex justify-center items-center h-screen'>
-          <div className="image-container">
-              <img src={profilePicture} alt='Profile' className='profile-pic'/>
-          </div>
-        
-        <p>
-          This is testing my name.
-        </p>
-      </section>
-    );
-  }
-  
-  export default Home;
+  return (
+    <section className="flex justify-center items-center h-screen">
+      
+      <div className="image-container">
+        <img src={profilePicture} alt="Profile" className="profile-pic" />
+      </div>
+
+      <p className="pl-6 text-3xl">Welcome! My name is Chris.</p>
+    
+    </section>
+  );
+}
+
+export default Home;
