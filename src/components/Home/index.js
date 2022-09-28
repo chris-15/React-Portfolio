7988abd1ee1f06f8948f8573e938198bae2144fb
Chris <christopher.sarm15@gmail.com>
2022-09-28 17:38:06 -0400

    more page styling

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,13 +3,15 @@ import profilePicture from "../../assets/images/chris_pic.jpg";
 
 function Home() {
   return (
-    <section className="flex justify-center items-center h-screen">
-      
-      <div className="image-container">
+    <section className="flex justify-center items-center h-screen flex-wrap
+    ">
+      <h1 className="text-5xl">Welcome! My name is Chris.</h1>
+
+      <div className="image-container ml-6">
         <img src={profilePicture} alt="Profile" className="profile-pic" />
       </div>
 
-      <h1 className="pl-6 text-5xl">Welcome! My name is Chris.</h1>
+      
     
     </section>
   );
