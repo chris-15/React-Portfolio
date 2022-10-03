782fc4d40e07c4ea795ec5a1f8329c665b59498b
Chris <christopher.sarm15@gmail.com>
2022-10-02 20:32:51 -0400

    restyled and refactored home page and changed picture

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,17 @@
 import React from "react";
-import profilePicture from "../../assets/images/chris_pic.jpg";
+
+import profilePicture from "../../assets/images/chris2.jpeg";
 
 function Home() {
   return (
-    <section className="flex justify-center items-center h-screen flex-wrap
-    ">
-      <h1 className="text-5xl">Welcome! My name is Chris.</h1>
+    <section className="">
+      <div className="max-w-[2000px] flex justify-center items-center h-screen">
+        <h1 className="text-5xl">Welcome! My name is <span className="text-white">Chris</span>.</h1>
 
-      <div className="image-container ml-6">
-        <img src={profilePicture} alt="Profile" className="profile-pic" />
+        <div className="ml-6 w-72 h-60 ">
+          <img src={profilePicture} alt="Profile" className="profile-pic rounded-full" />
+        </div>
       </div>
-
-      
-    
     </section>
   );
 }
