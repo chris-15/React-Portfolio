ad2e2bda34606c8eae35a3b197d045a9eda2e812
Chris <christopher.sarm15@gmail.com>
2022-10-02 22:59:45 -0400

    minor styling on home page for mobile

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,7 +7,7 @@ function Home() {
     <section className="max-w-[2000px] flex justify-center items-center h-screen">
         <h1 className="text-5xl">Welcome! My name is <span className="text-white animate-pulse">Chris</span>.</h1>
 
-        <div className="ml-6 w-72 h-60 ">
+        <div className="ml-6 mt-24 sm:mt-0 w-72 h-60 ">
           <img src={profilePicture} alt="Profile" className="profile-pic rounded-full" />
         </div>
       
