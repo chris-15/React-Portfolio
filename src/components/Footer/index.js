72f1beed06a9c3b7638852c4222d204d23addc1f
Chris <christopher.sarm15@gmail.com>
2022-10-02 22:03:25 -0400

    added pdf download of my resume to footer with an icon

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaFileAlt } from "react-icons/fa";
+import resume from '../../assets/documents/Sarmiento-Salas-Resume.pdf'
 
 function Footer() {
   return (
@@ -12,12 +13,22 @@ function Footer() {
         <FaGithub className=" text-6xl hover:bg-blue-500 p-2" />
       </a>
       <a
-        href="https://www.linkedin.com/"
+        href="https://www.linkedin.com/in/christopher-sarmiento-salas/"
         target="_blank"
         rel="noreferrer noopener"
       >
         <FaLinkedin className="text-6xl hover:bg-blue-500 p-2" />
       </a>
+
+      <a
+        href={resume}
+        target="_blank"
+        rel="noreferrer noopener"
+        download
+      >
+        <FaFileAlt className="text-6xl hover:bg-blue-500 p-2" />
+      </a>
+
     </footer>
   );
 }
